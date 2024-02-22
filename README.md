dcd271ca4ef973d580fd276045db3c2655aaf3fb
Daniel Töpfl <daniel.toepfl@squeezey.eu>
2024-02-22 15:03:50 +0100

    hi

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # dotfiles
-NixOS Dotfiles
+My NixOS Dotfiles
+
+Useful commands:
+Restart pipewire: systemctl --user restart pipewire.service
+Edit nixos config: sudo hx /etc/nixos/configuration.nix 
+Rebuild nixos: sudo nixos-rebuild switch
+
+Bindings:
+[SUPER] + [D] Open "dmenu"
+[SUPER] + [1..x] Change workspace
+[SUPER] + [SHITFT] + [1..x] Move windows to another workspace
+[SUPER] + [arrows] Change windows
+[SUPER] + [C] Close window
+[SUPER] + [V] Switch to float window
+[SUPER] + [S] Open a magic float workspace thingy
+[SUPER] + [SHIFT] + [S] Switch to magic float workspace thingy
+
+
+todo:
+- maybe change to homemanager?
---
+++ nixos/configuration.nix
@@ -0,0 +1,157 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "netdtpfl"; # Define your hostname.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Audio
+  security.rtkit.enable = true;
+	services.pipewire = {
+	 enable = true;
+	 alsa.enable = true;
+	 alsa.support32Bit = true;
+	 pulse.enable = true;
+	 # If you want to use JACK applications, uncomment this
+	 #jack.enable = true;
+	};
+
+  #git
+  programs.git = {
+    enable = true;
+    userName  = "dtpfl";
+    userEmail = "kontakt@dtpfl.de";
+  };
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "de_DE.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "de";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dtpfl = {
+    isNormalUser = true;
+    description = "dtpfl";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "dtpfl";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+    kitty
+    hyprpaper
+    waybar
+    microsoft-edge
+    helix
+    spotify
+    neofetch
+    obs-studio
+    tofi
+    vesktop
+    remmina
+    rustc
+    rust-analyzer 
+    steam
+    teamspeak5_client
+    vscode
+    lldb_17
+    github-desktop
+    grim 
+    slurp
+  ];
+  programs.hyprland.enable = true;
+
+    fonts = {
+      packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk
+        noto-fonts-emoji
+        font-awesome
+        source-han-sans
+        open-sans
+      ];
+      fontconfig.defaultFonts = {
+        serif = [ "Noto Serif" "Source Han Serif" ];
+        sansSerif = [ "Open Sans" "Source Han Sans" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
